5cbc7bfd8cb5b1c66f81db49df19dca4480e27b0
Ian Mayo <ian@planetmayo.com>
2025-10-22 10:28:16 +0100

    feat: Add data-testid attributes to menu items for improved testability

--- packages/chat-ui/src/admin/components/CustomMenu.tsx
+++ packages/chat-ui/src/admin/components/CustomMenu.tsx
@@ -10,10 +10,25 @@ import TemplateIcon from '@mui/icons-material/Description';
 
 export const CustomMenu = () => (
   <Menu>
-    <Menu.DashboardItem />
-    <Menu.Item to="/forces" primaryText="Forces" leftIcon={<GroupIcon />} />
-    <Menu.Item to="/rooms" primaryText="Rooms" leftIcon={<RoomIcon />} />
-    <Menu.Item to="/templates" primaryText="Templates" leftIcon={<TemplateIcon />} />
-    <Menu.Item to="/overview/edit" primaryText="Overview" leftIcon={<OverviewIcon />} />
+    <Menu.DashboardItem data-testid="menu-dashboard" />
+    <Menu.Item
+      to="/forces"
+      primaryText="Forces"
+      leftIcon={<GroupIcon />}
+      data-testid="menu-forces"
+    />
+    <Menu.Item to="/rooms" primaryText="Rooms" leftIcon={<RoomIcon />} data-testid="menu-rooms" />
+    <Menu.Item
+      to="/templates"
+      primaryText="Templates"
+      leftIcon={<TemplateIcon />}
+      data-testid="menu-templates"
+    />
+    <Menu.Item
+      to="/overview/edit"
+      primaryText="Overview"
+      leftIcon={<OverviewIcon />}
+      data-testid="menu-overview"
+    />
   </Menu>
 );
